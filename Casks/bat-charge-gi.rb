8f9aebba18c146435f848ac4aa3100dd00856ca2
SeongGi <linux1547@hanmail.net>
2026-03-01 16:15:42 +0900

    Add bat-charge-gi cask

---
+++ Casks/bat-charge-gi.rb
@@ -0,0 +1,24 @@
+cask "bat-charge-gi" do
+  version "2.7.3"
+  sha256 "0da37816986ff124116bd05446ccf69c56f3dd8aa2e4b630ff38b67e1d83165c"
+
+  url "https://github.com/SeongGi/dev/releases/download/v#{version}/bat-charge-gi.dmg"
+  name "bat-charge-gi"
+  desc "Advanced battery management and calibration tool for Apple Silicon"
+  homepage "https://github.com/SeongGi/dev"
+
+  app "bat-charge-gi.app"
+
+  # Helper 데몬 삭제를 위한 uninstall 구문 (사용자 편의성 제공)
+  uninstall launchctl: "com.seonggi.bat-charge-gi.helper",
+            delete:    "/Library/PrivilegedHelperTools/com.seonggi.bat-charge-gi.helper"
+
+  zap trash: [
+    "~/Library/Preferences/com.seonggi.bat-charge-gi.plist",
+    "~/Library/Application Support/bat-charge-gi"
+  ]
+
+  caveats do
+    "이 앱은 배터리 제어를 위해 루트 권한 백그라운드 헬퍼(SMAppService)를 사용합니다. 최초 실행 시 '코어 업데이트'를 통한 권한 허용이 필요합니다."
+  end
+end
