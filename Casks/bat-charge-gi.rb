491b90e9d2a3ce11d7db347cf104e88d17e34e40
SeongGi <linux1547@hanmail.net>
2026-03-14 09:02:10 +0900

    release: v2.8.6

--- Casks/bat-charge-gi.rb
+++ Casks/bat-charge-gi.rb
@@ -1,6 +1,6 @@
 cask "bat-charge-gi" do
-  version "2.8.5"
-  sha256 "20e07c52b2d94c6bc7b0432cbafa66d1b58dd7b91eb25c09bb62ee4fd34731ac"
+  version "2.8.6"
+  sha256 "c7ea0f979c501f608ddebd862e6949ef10efece2c6bb02e1659b970479d5375a"
 
   url "https://github.com/SeongGi/bat-charge-gi/releases/download/v#{version}/bat-charge-gi.dmg"
   name "bat-charge-gi"
@@ -18,7 +18,15 @@ cask "bat-charge-gi" do
     "~/Library/Application Support/bat-charge-gi"
   ]
 
+  postflight do
+    system_command "xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/bat-charge-gi.app"],
+                   sudo: false
+  end
+
   caveats do
-    "이 앱은 배터리 제어를 위해 루트 권한 백그라운드 헬퍼(SMAppService)를 사용합니다. 최초 실행 시 '코어 업데이트'를 통한 권한 허용이 필요합니다."
+    "이 앱은 배터리 제어를 위해 루트 권한 백그라운드 헬퍼(SMAppService)를 사용합니다.\n" +
+    "최초 실행 시 '백그라운드 제어 권한 허용' 버튼을 눌러 승인이 필요합니다.\n" +
+    "Gatekeeper 차단을 자동으로 해제하도록 설정되었습니다."
   end
 end
